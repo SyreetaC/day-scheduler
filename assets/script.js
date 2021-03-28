ab696151805fbccf352ea4c30238cf6fb071b5d3
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-03-28 13:16:59 +0100

    commented set colour logic

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,15 @@ const timeOfDay = document.querySelector("#past");
 $(document).ready(function () {
   $(currentDay).text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
 });
+
+//set text area colours based on time.
+const setTextAreaColour = () => {
+  //Get the hour from moment
+  const currentTimeString = moment().format("H");
+
+  //Take that hour and turn it into a string
+
+  //if string matches string from hour string, then change class to present.
+  //if string is > string from hour string, then change class to future.
+  //else keep as past.
+};
