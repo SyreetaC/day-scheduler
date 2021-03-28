c39a6265acf7e67151d3a7980e71bfc0c502ab77
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-03-28 14:13:42 +0100

    added event listener for set text colour fn

--- assets/script.js
+++ assets/script.js
@@ -7,20 +7,20 @@ $(document).ready(function () {
 });
 
 //set text area colours based on time.
-const setTextAreaColour = () => {
+const setTextAreaColour = (index) => {
   //Get the hour from moment
   currentTimeString = moment().format("H");
-  console.log(currentTimeString);
   const currentTimeNumber = parseInt(currentTimeString);
-
-  //Take that hour and turn it into a string
-
-  //get values of text areas for comparison
-  let timeNumbers = $("textarea").attr("data-time");
 };
+
+//Take that hour and turn it into a string
+
+//get values of text areas for comparison
 
 //if string matches string from data-time, then change class to present.
 //if string is > string from data-time, then change class to future.
 //else keep as past.
 
 // $("textarea").each(function ()
+
+$(document).ready(setTextAreaColour);
