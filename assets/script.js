f3b956c6e527dcc168c0750e7fbf99da68864f12
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-03-28 14:06:58 +0100

    wip

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 const currentDay = document.getElementById("currentDay");
 const timeOfDay = document.querySelector("#past");
+const allTextAreas = document.getElementsByTagName("textarea");
 
 $(document).ready(function () {
   $(currentDay).text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
@@ -8,11 +9,18 @@ $(document).ready(function () {
 //set text area colours based on time.
 const setTextAreaColour = () => {
   //Get the hour from moment
-  const currentTimeString = moment().format("H");
+  currentTimeString = moment().format("H");
+  console.log(currentTimeString);
+  const currentTimeNumber = parseInt(currentTimeString);
 
   //Take that hour and turn it into a string
 
-  //if string matches string from data-time, then change class to present.
-  //if string is > string from data-time, then change class to future.
-  //else keep as past.
+  //get values of text areas for comparison
+  let timeNumbers = $("textarea").attr("data-time");
 };
+
+//if string matches string from data-time, then change class to present.
+//if string is > string from data-time, then change class to future.
+//else keep as past.
+
+// $("textarea").each(function ()
