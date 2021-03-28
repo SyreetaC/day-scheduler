e8edf38e8bdd3c3c65f59fe3405c936e8796dfba
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-03-28 21:12:30 +0100

    set tasks into local memory

--- assets/script.js
+++ assets/script.js
@@ -11,11 +11,9 @@ const setTextAreaColour = (index) => {
   currentTimeString = moment().format("H");
   const currentTimeNumber = parseInt(currentTimeString);
   //Take that hour and turn it into a string
-  console.log(currentTimeNumber);
 
   //get values of text areas for comparison
   let scheduleTime = $(allTextAreas[index]).attr("data-time");
-  console.log(scheduleTime);
 
   //Compare the values of the current time and the time blocks on the schedule
   if (currentTimeNumber === scheduleTime) {
@@ -32,12 +30,59 @@ const setTextAreaColour = (index) => {
 $(allTextAreas).each(setTextAreaColour);
 
 //check if anything is in local storage- else return whatever was previously in local storage
-//set up local storage with empty objects
+const setUpLocalMemory = () => {
+  const dayScheduleMemory = localStorage.getItem("day-schedule");
+  if (dayScheduleMemory === null) {
+    console.log("Set up local memory");
+    const schedule = {
+      9: {
+        task: "",
+      },
+      10: {
+        task: "",
+      },
+      11: {
+        task: "",
+      },
+      12: {
+        task: "",
+      },
+      13: {
+        task: "",
+      },
+      14: {
+        task: "",
+      },
+      15: {
+        task: "",
+      },
+      16: {
+        task: "",
+      },
+      17: {
+        task: "",
+      },
+    };
+    //set up local storage with empty objects
+    const scheduleString = JSON.stringify(schedule);
+    localStorage.setItem("schedule", scheduleString);
+  } else {
+    console.log("memory exists!");
+  }
+};
+
+const setTaskIntoMemory = () => {
+  let task = allTextAreas[index].value;
+  console.log(task);
+  // const scheduleMemory = localStorage;
+};
 
 //link save button to local storage
 //remember that favicon is a child of the button!
 //set interval so (set text area colour function) colours automatically change after a particular time.
 
 $(document).ready(setTextAreaColour);
+$(document).ready(setUpLocalMemory);
+$(document).ready(setTaskIntoMemory);
 
 // document.ready for every function
