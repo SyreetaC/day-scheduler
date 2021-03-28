b65cc454c2a3d76c06dc7f06e8ae5c2a3272f4d1
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-03-28 13:05:17 +0100

    changed format of date

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 const currentDay = document.getElementById("currentDay");
+const timeOfDay = document.querySelector("#past");
 
 $(document).ready(function () {
-  $(currentDay).text(moment());
+  $(currentDay).text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
 });
