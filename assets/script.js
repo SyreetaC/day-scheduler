ec9d916dbc502854a1552c156db0d3fcdca7643c
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-03-28 19:41:55 +0100

    for loop added , jquery used to target HTML els

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,12 @@
-const currentDay = document.getElementById("currentDay");
-// const timeOfDay = document.querySelector("#past");
-const allTextAreas = document.getElementsByTagName("textarea");
+const currentDay = $("#currentDay");
+const allTextAreas = $(".container").children().children("textarea");
 
 $(document).ready(function () {
   $(currentDay).text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
 });
 
 //set text area colours based on time.
-const setTextAreaColour = () => {
+const setTextAreaColour = (index) => {
   //Get the hour from moment
   currentTimeString = moment().format("H");
   const currentTimeNumber = parseInt(currentTimeString);
@@ -15,25 +14,30 @@ const setTextAreaColour = () => {
   console.log(currentTimeNumber);
 
   //get values of text areas for comparison
-  let scheduleTime = $(allTextAreas).attr("data-time");
+  let scheduleTime = $(allTextAreas[index]).attr("data-time");
   console.log(scheduleTime);
 
   //Compare the values of the current time and the time blocks on the schedule
-  if (currentTimeString === scheduleTime) {
-    $(allTextAreas).addClass("present");
-  } else if (currentTimeString < scheduleTime) {
-    $(allTextAreas).addClass("future");
-  } else if (currentTimeString > scheduleTime) {
-    $(allTextAreas).addClass("past");
+  if (currentTimeNumber === scheduleTime) {
+    $(allTextAreas[index]).addClass("present");
+  } else if (currentTimeNumber < scheduleTime) {
+    $(allTextAreas[index]).addClass("future");
+  } else if (currentTimeNumber > scheduleTime) {
+    $(allTextAreas[index]).addClass("past");
   }
   //if string matches string from data-time, then change class to present.
   //if string is > string from data-time, then change class to future.
   //else keep as past.
 };
+$(allTextAreas).each(setTextAreaColour);
 
-//TO WORK ON- FOR EACH METHOD
-const setColours = () => {
-  allTextAreas.each(setTextAreaColour);
-};
+//check if anything is in local storage- else return whatever was previously in local storage
+//set up local storage with empty objects
+
+//link save button to local storage
+//remember that favicon is a child of the button!
+//set interval so (set text area colour function) colours automatically change after a particular time.
 
 $(document).ready(setTextAreaColour);
+
+// document.ready for every function
